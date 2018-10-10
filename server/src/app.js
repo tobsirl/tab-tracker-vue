89cf08d3d526df1f27b37681217ebce78e1d5848
Paul Tobin <tobster111@gmail.com>
2018-10-10 10:25:22 +0100

    Added Services - Api to the frontend

--- server/src/app.js
+++ server/src/app.js
@@ -5,13 +5,14 @@ const morgan = require('morgan');
 
 const app = express();
 
-app.use(morgan('conbine'));
+// Middleware
+app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'Hello World!',
+    message: `Your ${req.body.email}! user was registered! Have Fun`,
   });
 });
 
